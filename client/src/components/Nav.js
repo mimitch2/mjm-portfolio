42008b8368f9ec063fa5ae5354b924fa375f3e3b
mimitch2 <mike@melodix.com>
2018-06-25 10:13:55 -0500

    got link underline animations working

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,22 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react'
 // import PropTypes from 'prop-types'
 import '../css/App.css'
-import Diamond from './Diamond'
 
-const Nav = () => {
-  return (
-    <div className="nav-main">
-      <nav>
 
-        <ul className="nav-list">
-          <li className="nav-item">DESIGN</li>
-          <span className="star"></span>
-          <li className="nav-item">CODE</li>
-        </ul>
-  
-      </nav>
-    </div>
-  )
+
+class Nav extends Component{
+  state = {
+    selected: "nav-item selected",
+    unselected: "nav-item"
+  }
+
+ handleClick = (e) => {
+   const liElements = document.querySelectorAll('li')
+   liElements.forEach(li => li.className = this.state.unselected)
+   e.target.className = this.state.selected
+ }
+
+
+ render(){
+   return (
+     <div className="nav-main">
+       <nav>
+         <ul className="nav-list">
+           <li className={this.state.selected} onClick={(e) => this.handleClick(e)}>ABOUT</li>
+           <li className={this.state.unselected} onClick={(e) => this.handleClick(e)}>CODE</li>
+           <li className={this.state.unselected} onClick={(e) => this.handleClick(e)}>DESIGN</li>
+           <li className={this.state.unselected} onClick={(e) => this.handleClick(e)}>BLOG</li>
+         </ul>
+       </nav>
+     </div>
+   )
+ }
 }
 
 // Nav.propTypes = {
