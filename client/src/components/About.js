917e7e080c403d154151e94932d45a504acdd6dd
mimitch2 <mike@melodix.com>
2018-06-30 16:49:48 -0500

    checron animation done

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react'
 import '../css/App.css'
 import Fade from 'react-reveal/Fade';
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+
 
 class About extends Component {
 
@@ -28,15 +31,23 @@ class About extends Component {
         {/* <Fade> */}
         
         <div className="about-wrapper">
+       
           <h1 className="hello-text">HELLO</h1>
+
+         
           <div className="image-div austin-image">
-            {/* {this.renderImage()} */}
+       
           </div>
+          <div className="chevron-div"> <i className="far fa-chevron-double-down"></i></div>
+
           <div className="about-content">
-            Scroll Up and Down this page to see the parallax scrolling effect.
+     
+             Scroll Up and Down this page to see the parallax scrolling effect.
           </div>
           <div className="image-div js-image">
-            {/* {this.renderImage()} */}
+          </div>
+          <div className="about-content">
+             Scroll Up and Down this page to see the parallax scrolling effect.
           </div>
         </div>
         {/* </Fade> */}
