560f35b3510e2096ed5a1977256880eacc321d35
Mike Mitchell <mikemitchell@Mikes-Upswing-MacBook-Pro.local>
2018-07-18 16:09:37 -0500

    started on grid

--- client/src/components/Design.js
+++ client/src/components/Design.js
@@ -3,7 +3,29 @@ import '../css/App.css'
 
 const Design = () => {
   return (
-    <div className="design-main comp">DESIGN</div>
+    <div className="design-container container">
+      <div className="item item-1">item1</div>
+      <div className="item item-2">item2</div>
+      <div className="item item-3">item3</div>
+      <div className="item item-4">item4</div>
+      <div className="item item-5">item5</div>
+      <div className="item item-6">item6</div>
+      <div className="item item-7">item7</div>
+      <div className="item item-8">item8</div>
+      <div className="item item-9">item9</div>
+      <div className="item item-10">item10</div>
+      <div className="item item-11">item11</div>
+      <div className="item item-12">item12</div>
+      <div className="item item-13">item13</div>
+      <div className="item item-14">item14</div>
+      <div className="item item-15">item15</div>
+      <div className="item item-16">item16</div>
+      <div className="item item-17">item17</div>
+      <div className="item item-18">item18</div>
+      <div className="item item-19">item19</div>
+      <div className="item item-20">item20</div>
+    
+    </div>
   )
 }
 
