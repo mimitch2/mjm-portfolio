e3ac7e55dae5d6bedd3bdf64787ad014044f72a0
mimitch2 <mike@melodix.com>
2018-07-22 15:37:32 -0500

    got js hovering working in portfolio

--- client/src/components/Portfolio.js
+++ client/src/components/Portfolio.js
@@ -24,7 +24,16 @@ class Portfolio extends Component {
     return (
       <div className="portfolio-main site-cards-main">
         <div className={this.state.cardCss}>
-          <SiteCard image={<img src="img/cocktail_site.png" alt="cocktail website" className="cocktail-card-image card-image"/>} id="card-overlay-cocktail"/>
+          <SiteCard image={<img src="img/cocktail_site.png" alt="cocktail website" className="cocktail-card-image card-image"/>} heading="COCKTAIL APP"
+            info="A React/Redux based app that provides cocktail recipes."
+            icons={  
+              <div className="portfolio-icon-div">
+                <i className="fab port-icon fa-html5"></i>
+                <i className="fab port-icon fa-js-square"></i>
+                <i className="fab port-icon fa-css3-alt"></i>
+                <i className="fab port-icon fa-node"></i>
+                <i className="fab port-icon fa-react"></i>
+              </div>} />
         </div>
         <div className={this.state.cardCss}>
           <SiteCard image={<img src="img/old_portfolio_site.png" alt="portfolio website" className="portfolio-card-image card-image"/>} id="card-overlay-portfolio"/>
--- client/src/components/SiteCard.js
+++ client/src/components/SiteCard.js
@@ -6,11 +6,25 @@ import '../css/App.css'
 // this.classList.toggle("card-overlay")
 // this.classList.contains("foo")
 
+const addShow = (e) => {
+  e.target.classList.add("show")
+}
+const removeShow = (e) => {
+  e.target.classList.remove("show")
+}
+
 const SiteCard = (props) => {
     
   return (
     <div className="site-card">
-      <div className={`card-overlay ${props.id} basic-flex-row`}>
+      <div className={`card-overlay basic-flex-col`} onMouseEnter={addShow} onMouseLeave={removeShow}>
+        <span className="overlay-header">
+          {props.heading}
+        </span>
+        <span className="overlay-info">
+          {props.info}
+        </span>
+        {props.icons}
       </div>
       {props.image}
     </div>
