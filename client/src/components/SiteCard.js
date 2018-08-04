30a4754d4613017588e1567160ae0331ba596564
mimitch2 <mike@melodix.com>
2018-08-04 10:05:09 -0500

    simplified card overlays

--- client/src/components/SiteCard.js
+++ client/src/components/SiteCard.js
@@ -20,19 +20,20 @@ const SiteCard = (props) => {
   }
   // onMouseOver={addShow} onMouseLeave={removeShow}
   return (
-    <div className="site-card" >
-      <div className="card-overlay basic-flex-col"  >
-        <i className="fas fa-arrow-alt-circle-right" onClick={handleClick}></i> 
+    <div className="site-card">
+    
+      <div className="card-overlay basic-flex-col" onClick={handleClick}>
+        <div className="overlay-heading">
+          {props.heading}
+        </div>
+        <div className="overlay-info">
+          {props.info}
+          {props.icons}
+        </div>
       </div>
    
       <img src={props.image} alt="website" className="card-image"/>    
-      <div className="overlay-heading">
-        {props.heading}
-      </div>
-      <div className="overlay-info">
-        {/* {props.info} */}
-        {props.icons}
-      </div>
+ 
     </div>
   )
 }
